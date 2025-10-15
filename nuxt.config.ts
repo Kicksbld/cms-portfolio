14cd842ae26764a8d481fa29667cb502dc4feec2
killian <killian.boularand@outlook.fr>
2025-10-15 13:48:35 +0200

    🧑‍💻 Prisma, supabase and shadcn config setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@prisma/nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './app/components/ui'
+  },
 })
